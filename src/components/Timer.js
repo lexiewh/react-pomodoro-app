e7c482c77b14310349a4b07ed7bcc5c88a706854
LexieWhite <lexwhite028@gmail.com>
2020-03-10 17:14:32 -0500

    Countdown is working with cycles -> #5

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,58 +5,109 @@ import { Grid, Header, Button } from 'semantic-ui-react'
 export default class Timer extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      prodMinutes: Number(this.props.match.params.productiveTime),
-      shortMinutes: Number(this.props.match.params.shortBreak),
-      longMinutes: Number(this.props.match.params.longBreak),
-      seconds: 59,
-      session: 'Pomodoro'
+      prod: Number(this.props.match.params.productiveTime),
+      short: Number(this.props.match.params.shortBreak),
+      long: Number(this.props.match.params.longBreak),
+      seconds: 0,
+      session: 'Pomodoro Session',
+      interval: 4
     }
 
-    this.countDown = this.countDown.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.format = this.format.bind(this)
     this.startTimer = this.startTimer.bind(this)
-    this.secondsToTime = this.secondsToTime.bind(this)
+    this.sessionSwitch = this.sessionSwitch.bind(this)
+    this.getSessionMinutes = this.getSessionMinutes.bind(this)
+    this.resetSessionMinutes = this.resetSessionMinutes.bind(this)
   }
 
-  handleChange(event) {
-    this.setState({
-      prodMinutes: event.targetvalue
-    })
-  }
-
-  secondsToTime(secs){
-    let hours = Math.floor(secs / (60 * 60));
-
-    let divisor_for_minutes = secs % (60 * 60);
-    let minutes = Math.floor(divisor_for_minutes / 60);
-
-    let divisor_for_seconds = divisor_for_minutes % 60;
-    let seconds = Math.ceil(divisor_for_seconds);
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    };
-    return obj;
+  format(time) {
+    const val = time.toString().length < 2 ? '0' + time.toString() : time
+    return (val)
   }
 
   startTimer() {
-    console.log(setInterval(this.countDown, 1000))
+    this.myInterval = setInterval(() => {
+      const { seconds, session } = this.state
+      let minutes = this.getSessionMinutes()
+
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1
+        }))
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          this.resetSessionMinutes()
+          this.sessionSwitch()
+        } else {
+          if (session === "Pomodoro Session") {
+            this.setState({
+              prod: this.state.prod - 1,
+              seconds: 59
+            })
+          }
+          if (session === "Short Break") {
+            this.setState({
+              short: this.state.short - 1,
+              seconds: 59
+            })
+          }
+          if (session === "Long Break") {
+            this.setState({
+              long: this.state.long - 1,
+              seconds: 59
+            })
+          }
+        }
+      }
+    }, 1000)
   }
 
-  countDown() {
-    // Remove one second, set state so a re-render happens.
-    let seconds = this.state.seconds - 1;
-    this.setState({
-      time: this.secondsToTime(seconds),
-      seconds: seconds,
-    });
+getSessionMinutes() {
+  const { session, prod, long, short } = this.state
+  if (session === "Pomodoro Session") {
+    return prod
+  }
+  if (session === "Short Break") {
+    return short
+  }
+  if (session === "Long Break") {
+    return long
+  }
+}
 
-    // Check if we're at zero.
-    if (seconds === 0) {
-      clearInterval(this.timer);
+resetSessionMinutes() {
+  const { session } = this.state
+
+  if (session === "Pomodoro Session") {
+    this.setState({prod: Number(this.props.match.params.productiveTime)})
+  }
+  if (session === "Short Break") {
+    this.setState({short: Number(this.props.match.params.shortBreak)})
+  }
+  if (session === "Long Break") {
+    this.setState({long: Number(this.props.match.params.longBreak)})
+  }
+}
+
+  sessionSwitch() {
+    const { session, interval } = this.state
+    if (session === "Pomodoro Session" && interval > 0) {
+      this.setState({ session: "Short Break"})
+    }
+    if (session === "Pomodoro Session" && interval === 0) {
+      this.setState({
+        interval: 4,
+        session: "Long Break"
+      })
+    }
+    if (session === "Short Break") {
+      this.setState({ session: "Pomodoro Session"})
+    }
+    if (session === "Long Break") {
+      this.setState({ session: "Pomodoro Session" })
     }
   }
 
@@ -68,12 +119,10 @@ export default class Timer extends React.Component {
         <Header as='h1' textAlign='center' className='timer-header'>
           {this.state.session}
         </Header>
-        <p className="minutes" handleChange={this.handleChange}>
-          {this.state.prodMinutes + 1}:
-        </p>
-        <p className="seconds" handleChange={this.handleChange}>
-          {this.state.seconds}
-        </p>
+
+        <p className="minutes">{this.getSessionMinutes()}:</p>
+        <p className="seconds">{this.format(this.state.seconds)}</p>
+
         <Button.Group size='large'>
           <Button onClick={this.startTimer}>Start</Button>
           <Button>Stop</Button>
