d10fa2d99f43c928376bffbae869249cd9083327
LexieWhite <lexwhite028@gmail.com>
2020-03-17 14:41:22 -0500

    Switch class TimeForm to stateless functional compnent with hooks

--- src/components/TimeForm.js
+++ src/components/TimeForm.js
@@ -1,91 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import { Form, Grid, Header, Image, Segment, Button, Popup } from 'semantic-ui-react'
-import { Link } from "react-router-dom";
+import { withRouter, useHistory } from "react-router-dom"
+import { useForm, Controller } from "react-hook-form"
 
 
-export default class TimeForm extends React.Component {
-  constructor(props) {
-    super(props)
+function TimeForm() {
+  const [prodTime, setProdTime] = useState('')
+  const [shortBreak, setShortBreak] = useState('')
+  const [longBreak, setLongBreak] = useState('')
+  const [interval, setInterval] = useState('')
+  const history = useHistory()
 
-    this.state = {
-      productiveTime: "",
-      shortBreak: "",
-      longBreak: "",
-      interval: ""
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(event) {
+  /*handleChange(event) {
     const {name, value} = event.target
     this.setState({ [name]: value })
+  }*/
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    history.push(`/timer/${prodTime}-${shortBreak}-${longBreak}-${interval}`)
   }
 
-  render() {
-    return(
-      <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
-        <Grid.Column style={{ maxWidth: 500 }}>
-          <Header as='h2' textAlign='center' className="form">
-            <Image src={logo}/> How do you want to work?
-          </Header>
-          <Form size='large'>
-            <Segment stacked>
+  return(
+    <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 500 }}>
+        <Header as='h2' textAlign='center' className="form">
+          <Image src={logo}/> How do you want to work?
+        </Header>
+        <Form size='large' onSubmit={handleSubmit}>
+          <Segment stacked>
 
-              <Form.Input
-                fluid
-                icon='time'
-                iconPosition='left'
-                name="productiveTime"
-                value={this.state.productiveTime}
-                onChange={this.handleChange}
-                placeholder="Productive Time"
-              />
+            <Form.Input
+              fluid
+              icon='time'
+              type='number'
+              iconPosition='left'
+              name="productiveTime"
+              value={prodTime}
+              onChange={e => setProdTime(e.target.value)}
+              placeholder="Productive Time (minutes)"
+            />
 
-              <Form.Input
-                fluid
-                icon='time'
-                iconPosition='left'
-                name="shortBreak"
-                value={this.state.shortBreak}
-                onChange={this.handleChange}
-                placeholder="Short Break"
-              />
+            <Form.Input
+              fluid
+              icon='time'
+              type='number'
+              iconPosition='left'
+              name="shortBreak"
+              value={shortBreak}
+              onChange={e => setShortBreak(e.target.value)}
+              placeholder="Short Break (minutes)"
+            />
 
-              <Form.Input
-                fluid
-                icon='time'
-                iconPosition='left'
-                name="longBreak"
-                value={this.state.longBreak}
-                onChange={this.handleChange}
-                placeholder="Long Break"
-              />
+            <Form.Input
+              fluid
+              icon='time'
+              type='number'
+              iconPosition='left'
+              name="longBreak"
+              value={longBreak}
+              onChange={e => setLongBreak(e.target.value)}
+              placeholder="Long Break (minutes)"
+            />
 
-              <Popup
-                trigger={<Form.Input
-                          fluid
-                          icon='redo'
-                          iconPosition='left'
-                          name="interval"
-                          value={this.state.interval}
-                          onChange={this.handleChange}
-                          placeholder="Interval"
-                          />}
-                content="Interval will be the amount of times you will have a short break before a long break."
-                position="right center"
-              />
+            <Popup
+              trigger={<Form.Input
+                        fluid
+                        icon='redo'
+                        type='number'
+                        iconPosition='left'
+                        name="interval"
+                        value={interval}
+                        onChange={e => setInterval(e.target.value)}
+                        placeholder="Interval"
+                        />}
+              content="Interval will be the amount of times you will have a short break before a long break."
+              position="right center"
+            />
 
-              <Link
-                to={`/timer/${this.state.productiveTime}-${this.state.shortBreak}-${this.state.longBreak}-${this.state.interval}`}
-              >
-                <Button fluid size='large' className="btn">Submit</Button>
-              </Link>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    )
-  }
+              <Button fluid size='large' type='submit' className="btn">Submit</Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  )
 }
+
+export default withRouter(TimeForm)
