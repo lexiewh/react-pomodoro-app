54a1197903ce880b93d3dbbb4000770e1a7e30bf
LexieWhite <lexwhite028@gmail.com>
2020-03-17 18:56:16 -0500

    Updates

---
+++ precache-manifest.0628d1d346dc753d881d931eef2d7841.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909eaa13cc0b29ba531dde8137b285e2",
+    "url": "/react-pomodoro-app/index.html"
+  },
+  {
+    "revision": "d70ad7291072ac72d699",
+    "url": "/react-pomodoro-app/static/css/2.14de4e8a.chunk.css"
+  },
+  {
+    "revision": "1c65547c8075d2f3a671",
+    "url": "/react-pomodoro-app/static/css/main.26f6e4e2.chunk.css"
+  },
+  {
+    "revision": "d70ad7291072ac72d699",
+    "url": "/react-pomodoro-app/static/js/2.b5244da5.chunk.js"
+  },
+  {
+    "revision": "d4933a84b59a07c8e98247a034faf970",
+    "url": "/react-pomodoro-app/static/js/2.b5244da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c65547c8075d2f3a671",
+    "url": "/react-pomodoro-app/static/js/main.b4aa7276.chunk.js"
+  },
+  {
+    "revision": "cf3c8c3524292abec866",
+    "url": "/react-pomodoro-app/static/js/runtime-main.8a404246.js"
+  },
+  {
+    "revision": "ae1577e872e4298c5d7f644368fc1e69",
+    "url": "/react-pomodoro-app/static/media/alert.ae1577e8.mp3"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-pomodoro-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-pomodoro-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-pomodoro-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-pomodoro-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-pomodoro-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-pomodoro-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-pomodoro-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-pomodoro-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-pomodoro-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-pomodoro-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-pomodoro-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "c2b885cd3f330f869d11304b3d3df849",
+    "url": "/react-pomodoro-app/static/media/logo.c2b885cd.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-pomodoro-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-pomodoro-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-pomodoro-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-pomodoro-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-pomodoro-app/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
